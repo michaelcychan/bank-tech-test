c3d296832508dc339d5a0d3a692f07b70d101272
Michael Chan <michaelcychan@gmail.com>
2022-07-24 22:23:14 +0100

    passed a simple transaction test, refacotring of 🎰 getBalance function

--- account.js
+++ account.js
@@ -1,7 +1,13 @@
 class Account {
-  balance() {
-    return 0;
-  }
+  constructor() {
+    this.balance = 0
+  };
+  deposit(money) {
+    this.balance += money;
+  };
+  getBalance() {
+    return this.balance;
+  };
 }
 
 module.exports = Account;
--- tests/account.spec.js
+++ tests/account.spec.js
@@ -4,7 +4,14 @@ describe('Account', () => {
   describe('initialisation', () => {
     it('initialises with 0 balance', () => {
       const account1 = new Account();
-      expect(account1.balance()).toBe(0);
+      expect(account1.getBalance()).toBe(0);
+    });
+  });
+  describe('Simple transactions', () => {
+    it('increases balance by 100 if 100 is deposited', () => {
+      const account1 = new Account();
+      account1.deposit(100);
+      expect(account1.getBalance()).toBe(100);
     })
   })
 })
