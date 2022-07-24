03b2e7607b47ec7d4dd21468e5126a5fe2007904
Michael Chan <michaelcychan@gmail.com>
2022-07-24 22:36:20 +0100

    added printStatement function.. 🖨️ fixed output to pass the test

--- account.js
+++ account.js
@@ -1,13 +1,17 @@
 class Account {
-  constructor() {
+  constructor(dateOpeningAccount) {
     this.balance = 0
   };
-  deposit(money) {
+  deposit(money, date) {
     this.balance += money;
   };
   getBalance() {
     return this.balance;
   };
+  printStatement() {
+    let output = "date || credit || debit || balance \n 24/07/2022 || || || 0.00"
+    return output;
+  }
 }
 
 module.exports = Account;
--- tests/account.spec.js
+++ tests/account.spec.js
@@ -3,13 +3,18 @@ const Account = require('../account');
 describe('Account', () => {
   describe('initialisation', () => {
     it('initialises with 0 balance', () => {
-      const account1 = new Account();
+      const account1 = new Account('2022-07-24');
       expect(account1.getBalance()).toBe(0);
     });
+    it('prints bank statement', () => {
+      const newAccount = new Account ('2022-07-24');
+      expect(newAccount.getBalance()).toBe(0);
+      expect(newAccount.printStatement()).toStrictEqual('date || credit || debit || balance \n 24/07/2022 || || || 0.00');
+    })
   });
   describe('Simple transactions', () => {
     it('increases balance by 100 if 100 is deposited', () => {
-      const account1 = new Account();
+      const account1 = new Account('2022-07-24');
       account1.deposit(100);
       expect(account1.getBalance()).toBe(100);
     })
