a0c435e9b2741208ed2119177cedc226e8b3a832
Michael Chan <michaelcychan@gmail.com>
2022-07-26 22:10:17 +0100

    Statement class tested with two transactions, test passed

--- statement.js
+++ statement.js
@@ -1,6 +1,26 @@
 class Statement {
-  static print() {
-    console.log('date || credit || debit || balance');
+  static print(transactions) {
+    console.log(this.#generateStatementContent(transactions));
+  }
+
+  static #generateStatementContent(transactions) {
+    const header = 'date || credit || debit || balance';
+    let contents = [];
+    transactions.forEach((transaction) => {
+      contents.push(
+        [
+          transaction.date.toLocaleDateString('en-GB'),
+          this.#numberToDisplay(transaction.in),
+          this.#numberToDisplay(transaction.out),
+          this.#numberToDisplay(transaction.balance)
+        ].join(" || ").replace(/\s+/g, " ") // removing double whitespace after joining
+      );
+    });
+    return (transactions.length === 0) ?  header : header + '\n' + contents.reverse().join("\n");
+  }
+  static #numberToDisplay(number) {
+    // return null if input is null, otherwise return number with 2 decimal places.
+    return (number === null) ? null : number.toFixed(2);
   }
 }
 
--- tests/statement.spec.js
+++ tests/statement.spec.js
@@ -6,5 +6,29 @@ describe('Statement', () => {
     console.log = jest.fn();
     Statement.print(accountDouble.getTransactions());
     expect(console.log).toHaveBeenCalledWith('date || credit || debit || balance');
-  })
+  });
+  it('prints bank statement with one transaction', () => {
+    const date1 = new Date(2022, 6, 25);
+    const accountDouble = {
+      getTransactions: () => [
+        {date: date1, in: 100, out: null, balance: 100  }
+      ]
+    }
+    console.log = jest.fn();
+    Statement.print(accountDouble.getTransactions());
+    expect(console.log).toHaveBeenCalledWith('date || credit || debit || balance\n25/07/2022 || 100.00 || || 100.00');
+  });
+  it('prints bank statement with two transaction', () => {
+    const date1 = new Date(2022, 6, 25);
+    const date2 = new Date(2022, 6, 26);
+    const accountDouble = {
+      getTransactions: () => [
+        {date: date1, in: 100.5, out: null, balance: 100.5},
+        {date: date2, in: null, out: 30.2, balance: 70.3}
+      ]
+    }
+    console.log = jest.fn();
+    Statement.print(accountDouble.getTransactions());
+    expect(console.log).toHaveBeenCalledWith('date || credit || debit || balance\n26/07/2022 || || 30.20 || 70.30\n25/07/2022 || 100.50 || || 100.50');
+  });
 })
